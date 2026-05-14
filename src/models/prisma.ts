56dbe3cd65284140d6e2813f9e2fe6eac56bcbe7
Sanchez-Santiago <ssanchez321@alumnos.iua.edu.ar>
2026-05-14 10:28:32 -0300

    fix: corregir import de node:dns para evitar TypeError en produccion

--- src/models/prisma.ts
+++ src/models/prisma.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import dns from 'node:dns';
+import * as dns from 'node:dns';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
